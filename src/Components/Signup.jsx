61d8fc990b641858a44550cc4466fc093e159d50
Abdullah Mohamed <3abdullah7mohamed@gmail.com>
2024-02-16 05:39:34 +0200

    loading screen added

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -11,7 +11,9 @@ const Signup = () => {
     let logPass=useRef("");
     let signUsername=useRef("");
     let signPass=useRef("");
+    let [loading,setLoading]=useState(false);
     function signUp(){
+        setLoading(true);
         setDup("");
         let newUser={
             tasks:[],
@@ -38,6 +40,7 @@ const Signup = () => {
         }).then((data)=>{
             if(data===null){
                 setDup("h");
+                setLoading(false)
                 return
             }
             console.log(data,"hi");
@@ -47,6 +50,7 @@ const Signup = () => {
     }
     function logIn(){
         setWrong("");
+        setLoading(true);
         fetch(`https://taskserver-iy1n.onrender.com/login/${logUsername.current.value}/${logPass.current.value}`).then((response)=>{
             return response.json();
         }).then((data)=>{
@@ -55,6 +59,7 @@ const Signup = () => {
                 store.updateCurrUser(data);
             }
             else{
+                setLoading(false);
                 setWrong("h");
             }
         });
@@ -63,7 +68,40 @@ const Signup = () => {
         <div>
             {dup!=="" &&<h1 style={{textAlign:"center"}}>Username Already Exists</h1>} 
             {wrong!=="" &&<h1 style={{textAlign:"center"}}>Wrong Username Or Password</h1>}
-            <div className="main"> 
+            {loading && <div className="loader">
+            <svg xmlns="http://www.w3.org/2000/svg" height="200px" width="200px" viewBox="0 0 200 200" class="pencil">
+                    <defs>
+                        <clipPath id="pencil-eraser">
+                            <rect height="30" width="30" ry="5" rx="5"></rect>
+                        </clipPath>
+                    </defs>
+                    <circle transform="rotate(-113,100,100)" stroke-linecap="round" stroke-dashoffset="439.82" stroke-dasharray="439.82 439.82" stroke-width="2" stroke="currentColor" fill="none" r="70" class="pencil__stroke"></circle>
+                    <g transform="translate(100,100)" class="pencil__rotate">
+                        <g fill="none">
+                            <circle transform="rotate(-90)" stroke-dashoffset="402" stroke-dasharray="402.12 402.12" stroke-width="30" stroke="hsl(223,90%,50%)" r="64" class="pencil__body1"></circle>
+                            <circle transform="rotate(-90)" stroke-dashoffset="465" stroke-dasharray="464.96 464.96" stroke-width="10" stroke="hsl(223,90%,60%)" r="74" class="pencil__body2"></circle>
+                            <circle transform="rotate(-90)" stroke-dashoffset="339" stroke-dasharray="339.29 339.29" stroke-width="10" stroke="hsl(223,90%,40%)" r="54" class="pencil__body3"></circle>
+                        </g>
+                        <g transform="rotate(-90) translate(49,0)" class="pencil__eraser">
+                            <g class="pencil__eraser-skew">
+                                <rect height="30" width="30" ry="5" rx="5" fill="hsl(223,90%,70%)"></rect>
+                                <rect clip-path="url(#pencil-eraser)" height="30" width="5" fill="hsl(223,90%,60%)"></rect>
+                                <rect height="20" width="30" fill="hsl(223,10%,90%)"></rect>
+                                <rect height="20" width="15" fill="hsl(223,10%,70%)"></rect>
+                                <rect height="20" width="5" fill="hsl(223,10%,80%)"></rect>
+                                <rect height="2" width="30" y="6" fill="hsla(223,10%,10%,0.2)"></rect>
+                                <rect height="2" width="30" y="13" fill="hsla(223,10%,10%,0.2)"></rect>
+                            </g>
+                        </g>
+                        <g transform="rotate(-90) translate(49,-30)" class="pencil__point">
+                            <polygon points="15 0,30 30,0 30" fill="hsl(33,90%,70%)"></polygon>
+                            <polygon points="15 0,6 30,0 30" fill="hsl(33,90%,50%)"></polygon>
+                            <polygon points="15 0,20 10,10 10" fill="hsl(223,10%,10%)"></polygon>
+                        </g>
+                    </g>
+                </svg>
+            </div>}
+            {!loading &&<div className="main"> 
                 	
 		        <input type="checkbox" id="chk" aria-hidden="true"/>
                 <div className="login">
@@ -83,7 +121,7 @@ const Signup = () => {
                         <button onClick={signUp}>Register</button>
                     </div>
 			    </div>
-	        </div>
+	        </div>}
         </div>
      );
 }
