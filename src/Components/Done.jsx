63788c36dff1d424198ea9f9cd67d57045fbb04f
Abdullah Mohamed <3abdullah7mohamed@gmail.com>
2024-03-05 00:33:14 +0200

    done section ascending

--- src/Components/Done.jsx
+++ src/Components/Done.jsx
@@ -9,9 +9,13 @@ const Done = () => {
     useEffect(()=>{
         let userongoing=store.currUser.done;
         console.log(userongoing);
-        let temp=userongoing.map((task,index)=>{
-            return <DoneCard doneTime={task.doneTime} time={task.time} color={task.color} content={task.content} key={index} id={task.id}/>
-        })
+        let temp=[];
+        // let temp=userongoing.map((task,index)=>{
+        //     return <DoneCard doneTime={task.doneTime} time={task.time} color={task.color} content={task.content} key={index} id={task.id}/>
+        // })
+        for(let i=userongoing.length-1;i>=0;i--){
+            temp.push(<DoneCard doneTime={userongoing[i].doneTime} time={userongoing[i].time} color={userongoing[i].color} content={userongoing[i].content} key={i} id={userongoing[i].id}/>)
+        }
         setDone(temp);
     },[store.currUser]);
     return ( 
